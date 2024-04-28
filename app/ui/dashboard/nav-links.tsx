8993153ec7a19b3bcf1b6d0f0002dcd232dba2e6
cn <1208900727@qq.com>
2024-04-28 14:45:37 +0800

    安装操作vercel的库

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -22,7 +22,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname ()
-  console.log(pathname);
+  // console.log(pathname);
   return (
     <>
       {links.map((link) => {
